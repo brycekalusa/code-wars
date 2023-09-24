6f70b0f0de1890ff816578460ca329aff0a3fbcf
Bryce Kalusa <brycekalusa.dev@gmail.com>
2023-09-23 20:52:00 -0700

    Remove Exclamation Marks

---
+++ 8 kyu/removeExclamations.js
@@ -0,0 +1,7 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+//Answer
+
+function removeExclamationMarks(s) {
+    return s.replace('!', '')
+  }
