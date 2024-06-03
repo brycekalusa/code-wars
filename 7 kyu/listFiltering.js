6b147d2e30f4ab3fc8b9ff90e8b38a3bfc391af3
Bryce Kalusa <143682645+brycekalusa@users.noreply.github.com>
2024-06-02 20:28:04 -0700

    Delete 7 kyu directory

--- 7 kyu/listFiltering.js
+++
@@ -1,7 +0,0 @@
-//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
-
-//Answer
-
-function filter_list(l) {
-    return l.filter(Number.isInteger)
-  }
--- 7 kyu/reverseWords.js
+++
@@ -1,8 +0,0 @@
-//Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
-
-//Answer
-
-function reverseWords(str) {
-    let reverseWordArr = str.split(' ').map(word => word.split('').reverse().join(''))
-    return reverseWordArr.join(' ')
-  }
